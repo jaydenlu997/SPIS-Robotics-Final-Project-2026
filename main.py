942dc3b762b68fa4b181dcd7cbf6b6cb8a2c0001
nicknmi <66812615+nicknmi@users.noreply.github.com>
2026-08-28 14:26:42 -0700

    added vertex remembering

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def main():
   LeftServo = Servo(servoObj=servo_kit.continuous_servo[left_servo_channel])
   RightServo = Servo(servoObj=servo_kit.continuous_servo[right_servo_channel])
 
+  lastTime = time.time()
+
   seenVertices = []
 
 
@@ -30,39 +32,39 @@ def main():
     while (True):
       img, direction = RunPICamera(camera=camera)
 
-      """
-      if direction == Direction.RIGHT or direction == Direction.LEFT:
-        seenVertex = False
-        for vertex in seenVertices:
-          if match_places_ignore_tape_rgb(vertex, img):
-            print("found visited vertex")
-            seenVertex = True
-            break
-      
-        if not seenVertex:
-          seenVertices.append(img)
+      currentTime = time.time()
 
-      time.sleep(0.2)
-      print(len(seenVertices))
-      """
+      if currentTime - lastTime > 0.5:
+
+        if direction == Direction.RIGHT or direction == Direction.LEFT:
+          seenVertex = False
+          for vertex in seenVertices:
+            if match_places_ignore_tape_rgb(vertex, img):
+              print("found visited vertex")
+              seenVertex = True
+              break
+        
+          if not seenVertex:
+            seenVertices.append(img)
+        
+        lastTime = currentTime
+        
 
   
-
       match direction:
-        case Direction.STRAIGHT:
-          LeftServo.SetThrottle(1.0)
-          RightServo.SetThrottle(-1.0) 
-        case Direction.RIGHT:
-          LeftServo.SetThrottle(-1.0)
-          RightServo.SetThrottle(0) 
+          case Direction.STRAIGHT:
+              LeftServo.SetThrottle(1.0)
+              RightServo.SetThrottle(-1.0) 
+          case Direction.RIGHT:
+              LeftServo.SetThrottle(1.0)
+              RightServo.SetThrottle(None) 
 
           #for vertex in seenVertices:
-           # print("found visited vertex")
-        case Direction.LEFT:
-          LeftServo.SetThrottle(-1.0)
-          RightServo.SetThrottle(1.0) 
+          # print("found visited vertex")
+          case Direction.LEFT:
+              LeftServo.SetThrottle(None)
+              RightServo.SetThrottle(-1) 
 
-          #print("found visited vertex")
 
 
       time.sleep(0.2)
