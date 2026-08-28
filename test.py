d72be6fed840b4bfeef40433a28728187ab0dbde
nicknmi <ant112@ucsd.edu>
2026-08-28 09:14:47 -0700

    updated servo test

--- test.py
+++ test.py
@@ -2,12 +2,17 @@ from adafruit_servokit import ServoKit
 
 
 servo_channel = 0
+servo_channel1 = 1
+
 
 servo_kit = ServoKit(channels=16)
 
 servo_kit.continuous_servo[servo_channel].set_pulse_width_range(1200, 1800)
+servo_kit.continuous_servo[servo_channel1].set_pulse_width_range(1200, 1800)
 try:
     while True:
-        servo_kit.continuous_servo[servo_channel].throttle = 1.0
+        servo_kit.continuous_servo[servo_channel].throttle = -1.0
+        servo_kit.continuous_servo[servo_channel1].throttle = 1.0
 finally:
     servo_kit.continuous_servo[servo_channel].throttle = None
+    servo_kit.continuous_servo[servo_channel1].throttle = None
