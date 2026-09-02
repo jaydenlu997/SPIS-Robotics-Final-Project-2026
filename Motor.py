9478cc41a1ef981d6b7205dd44644dc3805a4ac3
nicknmi <66812615+nicknmi@users.noreply.github.com>
2026-09-02 10:39:44 -0700

    add scale to motor.py

--- Motor.py
+++ Motor.py
@@ -2,13 +2,14 @@ from gpiozero import OutputDevice, PWMOutputDevice
 
 
 class DCMotor:
-    def __init__(self, in1: int, in2: int, pwm: int) -> None:
+    def __init__(self, in1: int, in2: int, pwm: int, scale: float) -> None:
         self.in1 = OutputDevice(in1)
         self.in2 = OutputDevice(in2)
         self.pwm = PWMOutputDevice(pwm)
+        self.scale = scale
 
     def move(self, speed: float) -> None:
-        speed = min(max(speed, -1.0), 1.0)
+        speed = min(max(speed, -1.0), 1.0) * self.scale
         if speed > 0.0:
             self.in1.on()
             self.in2.off()
--- thing.py
+++ thing.py
@@ -7,7 +7,7 @@ left = DCMotor(in1=16, in2=12, pwm=24)
 
 try:
   while True:
-    right.move(1.0)
+    right.move(0.9)
     left.move(1.0)
 except KeyboardInterrupt:
   pass
